1bb663b4d3da27c6e3309351fca19b7f0a70afe0
alexmagnum2d0 <84418276+alexmagnum2d0@users.noreply.github.com>
2023-05-27 17:26:42 +0300

    Update db_models.py

--- db_models.py
+++ db_models.py
@@ -10,20 +10,20 @@ class Languages(Base):
     __tablename__ = 'Languages'
 
     lang_id = Column(Integer, primary_key=True)
-    lang_iso3 = Column(String, nullable=False)
+    lang_iso3 = Column(String, nullable=False, unique=True)
 
 class LanguagesTranslates(Base):
     __tablename__ = 'LanguagesTranslates'
 
     lang_trans_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    ltrans_cont = Column(String)
+    ltrans_cont = Column(String, nullable=True)
 
 class LanguageLevels(Base):
     __tablename__ = 'LanguageLevels'
 
     llvl_id = Column(Integer, primary_key=True)
-    llvl_cefr = Column(String, nullable=False)
+    llvl_cefr = Column(String, nullable=False, unique=True)
 
 
 
@@ -31,27 +31,27 @@ class ContForms(Base):
     __tablename__ = 'ContForms'
 
     cform_id = Column(Integer, primary_key=True)
-    cform_name = Column(String, nullable=False)
+    cform_name = Column(String, nullable=False, unique=True)
 
 class ContFormsTranslates(Base):
     __tablename__ = 'ContFormsTranslates'
 
     cform_id = Column(Integer, ForeignKey('ContForms.cform_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    cftrans_cont = Column(String)
+    cftrans_cont = Column(String, nullable=True)
 
 class ContSizes(Base):
     __tablename__ = 'ContSizes'
 
     csize_id = Column(Integer, primary_key=True)
-    csize_name = Column(String, nullable=False)
+    csize_name = Column(String, nullable=False, unique=True)
 
 class ContSizesTranslates(Base):
     __tablename__ = 'ContSizesTranslates'
 
     csize_id = Column(Integer, ForeignKey('ContSizes.csize_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    cstrans_cont = Column(String)
+    cstrans_cont = Column(String, nullable=True)
 
 class ContSizeDescrTranslates(Base):
 
@@ -60,46 +60,46 @@ class ContSizeDescrTranslates(Base):
     csize_id = Column(Integer, ForeignKey('ContSizes.csize_id'), primary_key=True)
     llvl_id = Column(Integer, ForeignKey('LanguageLevels.llvl_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    csdtrans_cont = Column(String)
+    csdtrans_cont = Column(String, nullable=True)
 
 class ContTypes(Base):
     __tablename__ = 'ContTypes'
 
     ctype_id = Column(Integer, primary_key=True)
-    ctype_name = Column(String, nullable=False)
+    ctype_name = Column(String, nullable=False, unique=True)
 
 class ContTypesTranslates(Base):
     __tablename__ = 'ContTypesTranslates'
 
     ctype_id = Column(Integer, ForeignKey('ContTypes.ctype_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    cttrans_cont = Column(String)
+    cttrans_cont = Column(String, nullable=True)
 
 class ContGenres(Base):
     __tablename__ = 'ContGenres'
 
     cgenre_id = Column(Integer, primary_key=True)
-    cgenre_name = Column(String, nullable=False)
+    cgenre_name = Column(String, nullable=False, unique=True)
 
 class ContGenresTranslates(Base):
     __tablename__ = 'ContGenresTranslates'
 
     cgenre_id = Column(Integer, ForeignKey('ContGenres.cgenre_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    cgtrans_cont = Column(String)
+    cgtrans_cont = Column(String, nullable=True)
 
 class ContTags(Base):
     __tablename__ = 'ContTags'
 
     ctag_id = Column(Integer, primary_key=True)
-    ctag_name = Column(String, nullable=False)
+    ctag_name = Column(String, nullable=False, unique=True)
 
 class ContTagsTranslates(Base):
     __tablename__ = 'ContTagsTranslates'
 
     ctag_id = Column(Integer, ForeignKey('ContTags.ctag_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    cttrans_cont = Column(String)
+    cttrans_cont = Column(String, nullable=True)
 
 class Contents(Base):
     __tablename__ = 'Contents'
@@ -110,8 +110,8 @@ class Contents(Base):
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), nullable=False)
     llvl_id = Column(Integer, ForeignKey('LanguageLevels.llvl_id'), nullable=False)
     csize_id = Column(Integer, ForeignKey('ContSizes.csize_id'), nullable=False)
-    ctype_id = Column(Integer, ForeignKey('ContTypes.ctype_id'))
-    cgenre_id = Column(Integer, ForeignKey('ContGenres.cgenre_id'))
+    ctype_id = Column(Integer, ForeignKey('ContTypes.ctype_id'), nullable=True)
+    cgenre_id = Column(Integer, ForeignKey('ContGenres.cgenre_id'), nullable=True)
 
 class ContentsContTags(Base):
     __tablename__ = 'ContentsContTags'
@@ -121,33 +121,33 @@ class ContentsContTags(Base):
 
 
 
-class QnTypes(Base):
-    __tablename__ = 'QnTypes'
+class PractTypes(Base):
+    __tablename__ = 'PractTypes'
 
-    qtype_id = Column(Integer, primary_key=True)
-    qtype_name = Column(String, nullable=False)
+    ptype_id = Column(Integer, primary_key=True)
+    ptype_name = Column(String, nullable=False, unique=True)
 
-class QnTypesTranslates(Base):
-    __tablename__ = 'QnTypesTranslates'
+class PractTypesTranslates(Base):
+    __tablename__ = 'PractTypesTranslates'
 
-    qtype_id = Column(Integer, ForeignKey('QnTypes.qtype_id'), primary_key=True)
+    ptype_id = Column(Integer, ForeignKey('PractTypes.ptype_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    qttrans_cont = Column(String)
+    pttrans_cont = Column(String, nullable=True)
 
-class QnDepths(Base):
-    __tablename__ = 'QnDepths'
+class PractDepths(Base):
+    __tablename__ = 'PractDepths'
 
-    qdepth_id = Column(Integer, primary_key=True)
-    qdepth_name = Column(String, nullable=False)
+    pdepth_id = Column(Integer, primary_key=True)
+    pdepth_name = Column(String, nullable=False, unique=True)
 
-class Questions(Base):
-    __tablename__ = 'Questions'
+class Practices(Base):
+    __tablename__ = 'Practices'
 
-    qn_id = Column(Integer, primary_key=True)
+    pract_id = Column(Integer, primary_key=True)
     cont_id = Column(Integer, ForeignKey('Contents.cont_id'), nullable=False)
-    qn_cont = Column(Text, nullable=False)
-    qtype_id = Column(Integer, ForeignKey('QnTypes.qtype_id'))
-    qdepth_id = Column(Integer, ForeignKey('QnDepths.qdepth_id'))
+    pract_cont = Column(Text, nullable=False)
+    ptype_id = Column(Integer, ForeignKey('PractTypes.ptype_id'), nullable=True)
+    pdepth_id = Column(Integer, ForeignKey('PractDepths.pdepth_id'), nullable=True)
 
 
 
@@ -156,14 +156,14 @@ class Plans(Base):
     __tablename__ = 'Plans'
 
     plan_id = Column(Integer, primary_key=True)
-    plan_name = Column(String, nullable=False)
+    plan_name = Column(String, nullable=False, unique=True)
 
 class PlansTranslations(Base):
     __tablename__ = 'PlansTranslations'
 
     plan_id = Column(Integer, ForeignKey('Plans.plan_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    ptrans_cont = Column(String)
+    ptrans_cont = Column(String, nullable=True)
 
 class PlanPrices(Base):
     __tablename__ = 'PlanPrices'
@@ -171,7 +171,7 @@ class PlanPrices(Base):
     pprice_id = Column(Integer, primary_key=True)
     plan_id = Column(Integer, ForeignKey('Plans.plan_id'), nullable=False)
     currency = Column(String, nullable=False)
-    pprice_value = Column(Float)
+    pprice_value = Column(Float, nullable=True)
 
 class PlanLimits(Base):
     __tablename__ = 'PlanLimits'
@@ -185,23 +185,34 @@ class PlanLimitDescrTranslates(Base):
 
     plan_id = Column(Integer, ForeignKey('PlanLimits.plimit_id'), primary_key=True)
     cont_id = Column(Integer, ForeignKey('Contents.cont_id'), primary_key=True)
-    llvl_id = Column(Integer, ForeignKey('LanguageLevels.lang_id'), primary_key=True)
+    llvl_id = Column(Integer, ForeignKey('LanguageLevels.llvl_id'), primary_key=True)
     lang_id = Column(Integer, ForeignKey('Languages.lang_id'), primary_key=True)
-    pldtrans_cont = Column(String)
+    pldtrans_cont = Column(String, nullable=True)
 
 
 
-class Users(Base):
-    __tablename__ = 'Users'
+class Users(Base):  
+  __tablename__ = 'Users'  
 
-    user_id = Column(Integer, primary_key=True)
-    user_tg_id = Column(Integer, nullable=False)
+  user_id = Column(Integer, primary_key=True)  
+  user_tg_id = Column(Integer, nullable=False)
+  
+  registrations = relationship('UsersRegistrations', back_populates='user')
+  settings = relationship('UsersSettings', back_populates='user', uselist=False)
+  cont_tags = relationship('UsersContTags', back_populates='user')
+  payments = relationship('UsersPayments', back_populates='user')
+  limits = relationship('UserLimits', back_populates='user')
+  contents = relationship('UserContents', back_populates='user')
+  practices = relationship('UserPractices', back_populates='user')
 
 class UsersRegistrations(Base):
     __tablename__ = 'UsersRegistrations'
 
     user_id = Column(Integer, ForeignKey('Users.user_id'), nullable=False)
     ureg_date = Column(String, nullable=False)
+
+    user = relationship('Users', back_populates='registrations')
+
 
 class UsersSettings(Base):
     __tablename__ = 'UsersSettings'
@@ -212,7 +223,10 @@ class UsersSettings(Base):
     study_lang_id = Column(Integer, ForeignKey('Languages.lang_id'), nullable=False)
     study_llvl_id = Column(Integer, ForeignKey('LanguageLevels.llvl_id'), nullable=False)
     csize_id = Column(Integer, ForeignKey('ContSizes.csize_id'), nullable=False)
-    user_interest = Column(Text)
+    user_interest = Column(Text, nullable=True)
+
+    user = relationship('Users', back_populates='settings')
+
 
 class UsersContTags(Base):
     __tablename__ = 'UsersContTags'
@@ -220,14 +234,19 @@ class UsersContTags(Base):
     user_id = Column(Integer, ForeignKey('Users.user_id'), primary_key=True)
     ctag_id = Column(Integer, ForeignKey('ContTags.ctag_id'), primary_key=True)
 
+    user = relationship('Users', back_populates='settings')
+
 class UsersPayments(Base):
     __tablename__ = 'UsersPayments'
 
-    user_id = Column(Integer, ForeignKey('Users.user_id'), nullable=False)
-    plan_id = Column(Integer, ForeignKey('Plans.plan_id'), nullable=False)
+    user_id = Column(Integer, ForeignKey('Users.user_id'), primary_key=True)
+    plan_id = Column(Integer, ForeignKey('Plans.plan_id'), primary_key=True)
+    upay_iter = Column(Integer, primary_key=True)
     upay_date = Column(String, nullable=False)
     currency = Column(String, nullable=False)
     payment_value = Column(Float, nullable=False)
+
+    user = relationship('Users', back_populates='settings')
 
 class UserLimits(Base):
     __tablename__ = 'UserLimits'
@@ -236,6 +255,8 @@ class UserLimits(Base):
     ctype_id = Column(Integer, ForeignKey('ContTypes.ctype_id'), primary_key=True)
     ulimit_value = Column(Integer, nullable=False)
 
+    user = relationship('Users', back_populates='settings')
+
 class UserContents(Base):
     __tablename__ = 'UserContents'
 
@@ -243,24 +264,22 @@ class UserContents(Base):
     cont_id = Column(Integer, ForeignKey('Contents.cont_id'), primary_key=True)
     ucont_iter = Column(Integer, primary_key=True)
     cont_send_date = Column(String, nullable=False)
-    cont_difficult_rate = Column(Integer)
-    cont_interest_rate = Column(Integer)
+    cont_difficult_rate = Column(Integer, nullable=True)
+    cont_interest_rate = Column(Integer, nullable=True)
 
-class RespCorrects(Base):
-    __tablename__ = 'RespCorrects'
+    user = relationship('Users', back_populates='settings')
 
-    rcorr_id = Column(Integer, primary_key=True)
-    rcorr_name = Column(String, nullable=False)
-
-class UserQuestions(Base):
-    __tablename__ = 'UserQuestions'
+class UserResponses(Base):
+    __tablename__ = 'UserPractices'
 
     user_id = Column(Integer, ForeignKey('Users.user_id'), primary_key=True)
-    qn_id = Column(Integer, ForeignKey('Questions.qn_id'), primary_key=True)
-    uqn_iter = Column(Integer, primary_key=True)
-    qn_send_time = Column(String, nullable=False)
-    uqn_resp = Column(String)
-    uqn_resp_time = Column(String)
-    rcorr_id = Column(Integer, ForeignKey('RespCorrects.rcorr_id'))
-    gpt_resp = Column(String)
-    gpt_resp_time = Column(String)
+    pract_id = Column(Integer, ForeignKey('Practices.pract_id'), primary_key=True)
+    uresp_iter = Column(Integer, primary_key=True)
+    pract_send_time = Column(String, nullable=False)
+    uresp_cont = Column(String, nullable=True)
+    uresp_resp_time = Column(String, nullable=True)
+    uresp_correct = Column(String, nullable=True)
+    gpt_resp = Column(String, nullable=True)
+    gpt_resp_time = Column(String, nullable=True)
+
+    user = relationship('Users', back_populates='practices')
